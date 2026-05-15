ba7d113f56d90941687154fa30b87a5bc970080a
wbowen123 <64280340+wbowen123@users.noreply.github.com>
2026-05-15 12:24:57 +0800

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+echo "即将执行一键卸载"
+echo "默认会自动删除服务器端节点和订阅配置"
+echo "客户端内已导入的节点和订阅仍需手动删除"
+
 wget -P /root -N --no-check-certificate "https://raw.githubusercontent.com/wbowen123/v2ray-agent/master/install.sh"
 chmod 700 /root/install.sh
 printf '20\ny\n' | /root/install.sh
